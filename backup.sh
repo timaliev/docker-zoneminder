e81d35fd337be69238a8d0ff1d94bef6594472e3
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2020-03-17 20:33:07 -0400

    Delete backup.sh

--- backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Backup mysql
-mysqldump -u root -pmysqlpsswd -h ${ZM_DB_HOST:-localhost} -P$ZM_DB_PORT --all-databases > /var/backups/alldb_backup.sql
-
-#Backup important file ... of the configuration ...
-cp  /etc/hosts  /var/backups/
-
