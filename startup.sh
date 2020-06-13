39be4d58412fdbb7e6e521998f08efb251d535bc
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2020-06-13 14:23:54 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -76,6 +76,7 @@ else
   # [ -f /var/cache/zoneminder/configured ]
   if [[ $EMPTYDATABASE != 0 ]]; then
         echo 'database already configured.'
+	zmupdate.pl
         rm -rf /var/run/zm/* 
 	/sbin/zm.sh&
    else  
